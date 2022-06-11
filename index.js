199b34ce63f5b44ad387fb5610724b310d24c6b3
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-11 11:59:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (..._drivers) => {
+
+    let result = drivers.filter((driver) => {
+        return driver.length > 6 || driver.length == 4;
+    });
+
+    return result;
+};
+
+const returnLastTwoDrivers = (..._drivers) => {
+
+    let result = drivers.filter((driver) => {
+        return driver.length == 5 || driver.length == 2;
+    });
+
+    return result;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const quintupler = 5;
+
+function createFareMultiplier(_quintupler) {
+
+    const fareQuintupler = () => {
+        let result = quintupler * 5;
+        return result;
+    }
+
+    return fareQuintupler;
+}
+
+const fareTimesTen = 10;
+
+const fareDoubler = (_fareTimesTen) => {
+    let result = fareTimesTen * 2;
+    return result;
+}
+
+const fareTimesTwelve = 12;
+
+const fareTripler = (_fareTimesTwelve) => {
+    let result = fareTimesTwelve * 3;
+    return result;
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers();
+}
